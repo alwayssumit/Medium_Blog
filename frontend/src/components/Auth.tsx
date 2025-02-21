f22b482e5912fefd9855ee81bc43153f66164dc1
alwayssumit <sanjayshuklacpr@gmail.com>
2025-02-21 21:02:44 +0530

    updated

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -7,6 +7,7 @@ import toast from "react-hot-toast";
 import { SignupInput } from "@codingprism/medium-common";
 
 
+
 export const Auth=({type}:{type:"signup"|"signin"})=>{
    const [postInputs,setpostInputs]=useState<SignupInput>({
     email:"",
